3dffcc60ebd8bc00c50dd9c8c396d70a9a7e8450
kodavalla75 <kodavalla75@gmail.com>
2024-12-17 13:08:08 +0530

    Second commit

--- src/sampleProject/Test1.java
+++ src/sampleProject/Test1.java
@@ -5,6 +5,16 @@ public class Test1 {
 	public static void main(String[] args) {
 		
 		
+		String actUrl = "https://www.google.com";
+		String expUrl = "https://www.Google.com";
+		
+		if(actUrl.equals(expUrl)) {
+			System.out.println("verified url");
+		}else
+		{
+			System.out.println("does not verified");
+		}
+		
 
 	}
 
